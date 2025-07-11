a9326f10b776f4423a9c6a6797816c1bc3e69c5e
h4m1dr <h4m1drn@example.com>
2025-07-11 17:07:56 +0200

    Updated multifon.sh with working menu and logic

--- multifon.sh
+++ multifon.sh
@@ -25,15 +25,6 @@ echo -e "                    ${WHITE}Multi Psiphon Manager${RESET}${CYAN}  | ${W
 echo ""
 }
 
-
-# Multi Psiphon logo
-logo() {
-    clear
-    echo -e "${MAGENTA}${BOLD}  __  __       _     _       _             "
-    echo -e fake logo
-    echo -e "${RESET}"
-}
-لوگو رو دست نزن
 # status and location count
 check_status() {
     [[ -f "/usr/bin/psiphon-tunnel-core-x86_64" ]] && psi_status="${GREEN}✓ Installed${RESET}" || psi_status="${RED}✗ Not Found${RESET}"
